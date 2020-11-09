111be8bc9164dfec906f9833fab9277c40c6a30d
Jurrian <jurrianlammerts@hotmail.com>
2020-11-09 14:48:18 +0100

    Added slick page animations

--- pages/_app.js
+++ pages/_app.js
@@ -3,30 +3,19 @@ import { motion, AnimatePresence } from 'framer-motion';
 
 import '../styles/App.scss';
 
+const spring = {
+  type: 'spring',
+  damping: 20,
+  stiffness: 100,
+  when: 'afterChildren',  
+};
+
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
-  const spring = {
-    type: 'spring',
-    damping: 20,
-    stiffness: 100,
-    when: 'afterChildren',
-  };
-
   return (
     <AnimatePresence>
-      <div className="page-transition-wrapper">
-        <motion.div
-          transition={spring}
-          key={router.pathname}
-          initial={{ x: 300, opacity: 0 }}
-          animate={{ x: 0, opacity: 1 }}
-          exit={{ x: -300, opacity: 0 }}
-          id="page-transition-container"
-        >
-          <Component {...pageProps} key={router.pathname} />
-        </motion.div>
-      </div>
+      <Component {...pageProps} key={router.pathname} />
     </AnimatePresence>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,61 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import { motion } from 'framer-motion';
+import ProgressiveImage from 'react-progressive-image';
+
 import Layout from '../components/Layout';
 
-export default function Home() {
-  return (
-    <Layout>
-      <Link href="/about">about</Link>
-    </Layout>
-  );
-}
+const transition = { duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96] };
+
+const Home = ({ image }) => (
+  <Layout>
+    <main>
+      <div className="container">
+        <div className="row center">
+          <div className="image-container">
+            <div
+              className="thumbnail"
+              ref={image}
+              style={{
+                width: 524,
+                height: 650,
+              }}
+            >
+              <Link href="/model/yasmeen-tariq">
+                <div className="frame">
+                  <ProgressiveImage
+                    src={'/images/yasmeen.webp'}
+                    placeholder={'/images/compressed-image.jpg'}
+                  >
+                    {(src) => (
+                      <motion.img
+                        src={src}
+                        alt="Yasmeen Tariq"
+                        initial={{ scale: 0.95 }}
+                        whileHover={{ scale: 1 }}
+                        transition={transition}
+                      />
+                    )}
+                  </ProgressiveImage>
+                </div>
+              </Link>
+            </div>
+            <motion.div
+              exit={{ opacity: 0 }}
+              transition={transition}
+              className="information"
+            >
+              <div className="title">Yasmeen Tariq</div>
+              <div className="location">
+                <span>28.538336</span>
+                <span>-81.379234</span>
+              </div>
+            </motion.div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </Layout>
+);
+
+export default Home;
